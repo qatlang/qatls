b8348ebe47771e2d647b8b7e813068a91423b616
Aldrin Mathew <admin@aldrinmathew.com>
2024-09-11 11:10:58 +0530

    Added `FileRange` and `Position`
    - Added `FileRange` and `Position` to represent positon and range
      in a file to be processed by the LSP
    
    - Added functions to check if a given position is within a file range
      or if the position occurs exactly after the range

--- types.go
+++ types.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	protocol "github.com/tliron/glsp/protocol_3_16"
 )
 
@@ -15,4 +17,37 @@ type File struct {
 	version protocol.Integer
 	content string
 	isOpen  bool
+}
+
+type Position struct {
+	Line      protocol.UInteger `json:"line"`
+	Character protocol.UInteger `json:"character"`
+}
+
+type FileRange struct {
+	Path  string   `json:"path"`
+	Start Position `json:"start"`
+	End   Position `json:"end"`
+}
+
+func (self FileRange) IsSamePathAs(pathVal string) bool {
+	selfPathInfo, err := os.Lstat(self.Path)
+	if err != nil {
+		Server.Log.Error("Error getting FileInfo of path: " + self.Path)
+		return false
+	}
+	pathValInfo, err := os.Lstat(pathVal)
+	if err != nil {
+		Server.Log.Error("Error getting FileInfo of path: " + pathVal)
+		return false
+	}
+	return os.SameFile(selfPathInfo, pathValInfo)
+}
+
+func (self FileRange) IsWithin(pathVal string, pos Position) bool {
+	return self.IsSamePathAs(pathVal) && ((self.Start.Line < pos.Line) && (self.End.Line > pos.Line)) || ((self.Start.Line == pos.Line) && (self.Start.Character <= pos.Character) && (self.End.Line == pos.Line) && (self.End.Character >= pos.Character)) || ((self.Start.Line == pos.Line) && (self.Start.Character <= pos.Character)) || ((self.End.Line == pos.Line) && (self.End.Character >= pos.Character))
+}
+
+func (self FileRange) IsRightAfter(pathVal string, pos Position) bool {
+	return self.IsSamePathAs(pathVal) && (self.End.Line == pos.Line) && ((self.End.Character + 1) == pos.Character)
 }
