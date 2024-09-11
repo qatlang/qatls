674e335ab3e4a32569d9c5ce87b57d1d5a3425d5
Aldrin Mathew <admin@aldrinmathew.com>
2024-09-11 10:56:18 +0530

    Added `QatSession` and `File` structs
    - Added `QatSession` to represent an LSP server session
    
    - Added `File` struct to represent a file to be processed by the
      LSP

--- types.go
+++ types.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	protocol "github.com/tliron/glsp/protocol_3_16"
+)
+
+type QatSession struct {
+	handler    protocol.Handler
+	workspaces []protocol.WorkspaceFolder
+	files      []File
+}
+
+type File struct {
+	uri     string
+	version protocol.Integer
+	content string
+	isOpen  bool
+}
